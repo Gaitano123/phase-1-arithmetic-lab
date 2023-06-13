254213dd6f737b05014eb007276fe392628068ff
Gaitano <gaitano.george@student.moringaschool.com>
2023-06-13 11:04:40 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+
+
+const multiply = (num1 * num2);
+console.log(multiply);
+// const random = (Math.floor(math.random()+1));
+// const mod = (19 % 5);
+// const max = (Math.max(2, 31, 5, 24));
+
+// console.log(multiply,random,mod,max)
+
+const random =  Math.ceil (Math.random()*20);
+
+let num3 = 24;
+let num4 = 5;
+
+const mod = (num3 % num4)
+
+const max = Math.max(18, 1, 20, 14)
